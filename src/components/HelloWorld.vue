974f6699c9bcb84fe3b9d89b4323c8044340373e
Guillaume <occuly.guillaume@gmail.com>
2019-03-21 23:26:59 +0100

    Vue.js with a Theme and Scoped CSS Styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,28 @@
+/* eslint-disable semi */
+/* eslint-disable semi */
 <template>
   <div>
-    <h1>Welcome to the Forum</h1>
-    <div v-for="thread in threads">
-      <h2>{{thread.title}}</h2>
-      <div v-for="postId in thread.posts">
-        <p>{{users[posts[postId].userId].name}}</p>
-        <p>{{posts[postId].text}}</p>
+    <div v-for="thread in threads" class="col-large push-top">
+      <h1>{{thread.title}}</h1>
+
+      <div class="post-list">
+        <div v-for="postId in thread.posts" class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+            <a href="#">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt>
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>{{posts[postId].text}}</div>
+          </div>
+
+          <div class="post-date text-faded">{{posts[postId].publishedAt}}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -13,16 +30,16 @@
 
 <script>
 // eslint-disable-next-line semi
-import sourceDate from '@/data.json';
+import sourceData from '@/data';
+console.log(sourceData)
 
-console.log(sourceDate)
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      threads: sourceDate.threads,
-      posts: sourceDate.posts,
-      users: sourceDate.users
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
     }
   }
 }
